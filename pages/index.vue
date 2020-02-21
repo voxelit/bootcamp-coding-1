8300602fbcefc5c951382860c7d1da97b584940a
Samantha Baita <samantha.baita@revas.io>
2020-02-21 10:14:18 +0100

    Setup progetto

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <img src="https://bit.ly/voxel-logo" />
+        </div>
+      </div>
+    </section>
+
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Voxel</h1>
+          <h2
+            class="subtitle"
+          >Una community di empowerement femminile dove poter crescere assieme tecnicamente e culturalmente
+          </h2>
+          <a href="mailto:nicolo.gardoni@revas.io" class="button is-primary">Mandaci una mail </a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  import Logo from "~/components/Logo.vue";
+
+  export default {
+    components: {
+      Logo
+    }
+  };
+</script>
+
+
